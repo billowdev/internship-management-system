2b43a1b4950308df1eb531f852e47276f0515614
Akkarapon Phikulsri <lacakp.contact@gmail.com>
2022-04-21 13:11:12 -0700

    admin manage login controllers

--- api/controllers/admin/login.controller.js
+++ api/controllers/admin/login.controller.js
@@ -1,18 +1,18 @@
 const { Login } = require("../../models");
-const {Op} = require('sequelize')
+const { Op } = require('sequelize')
 exports.createLogin = async (req, res) => {
 	try {
 		const isAdmin = await Login.findOne({ where: { id: req.user.id, permission: 'admin' } })
 		const { username, password, permission } = req.body;
 
 		if (isAdmin != null) {
-			const isExist = await Login.findOne({where:{username:username}})
+			const isExist = await Login.findOne({ where: { username: username } })
 
-			if(isExist==null){
+			if (isExist == null) {
 				const resp = await Login.create({ username, password, permission })
 				res.status(200).json({ success: true, msg: "create login success", data: resp })
-			}else{
-				res.status(201).json({ success: false, msg: "create login faild user is exist"})
+			} else {
+				res.status(201).json({ success: false, msg: "create login faild user is exist" })
 			}
 		} else {
 			res.status(404).json({ success: false, msg: "404 Not Found" })
@@ -34,64 +34,99 @@ exports.getLogin = async (req, res) => {
 			const sortDirection = req.query.sort_direction;
 			const search = req.query.search;
 			const startIndex = (page - 1) * perPage;
-	  
+
 			const total = await Login.count();
 			let totalPages = total / perPage;
 			let resp;
-	  
+
 			if (search && sortColumn) {
-			  resp = await Login.findAll({
-				offset: startIndex,
-				limit: perPage,
-				attributes: { exclude: ["password"] },
-				where: {
-				username: {
-					[Op.like]: `%${search}%`,
-				  },
-				},
-				order: [[sortColumn, sortDirection]],
-				raw: true,
-			  });
+				resp = await Login.findAll({
+					offset: startIndex,
+					limit: perPage,
+					attributes: { exclude: ["password"] },
+					where: {
+						username: {
+							[Op.like]: `%${search}%`,
+						},
+					},
+					order: [[sortColumn, sortDirection]],
+					raw: true,
+				});
 			} else if (search) {
-			  resp = await Login.findAll({
-				attributes: { exclude: ["password"] },
-				where: {
-				  username: {
-					[Op.like]: `%${search}%`,
-				  },
-				},
-				raw: true,
-			  });
+				resp = await Login.findAll({
+					attributes: { exclude: ["password"] },
+					where: {
+						username: {
+							[Op.like]: `%${search}%`,
+						},
+					},
+					raw: true,
+				});
 			} else if (sortColumn) {
-			  resp = await Login.findAll({
-				offset: startIndex,
-				limit: perPage,
-				attributes: { exclude: ["password"] },
-				order: [[sortColumn, sortDirection]],
-				raw: true,
-			  });
+				resp = await Login.findAll({
+					offset: startIndex,
+					limit: perPage,
+					attributes: { exclude: ["password"] },
+					order: [[sortColumn, sortDirection]],
+					raw: true,
+				});
 			} else {
-			  resp = await Login.findAll({
-				offset: startIndex,
-				limit: perPage,
-				attributes: { exclude: ["password"] },
-				raw: true,
-			  });
+				resp = await Login.findAll({
+					offset: startIndex,
+					limit: perPage,
+					attributes: { exclude: ["password"] },
+					raw: true,
+				});
 			}
 			res.status(200).json({
-			  success: true,
-			  msg: "get Login success",
-			  data: {
-				page: page,
-				per_page: perPage,
-				total_pages: totalPages,
-				total: total,
-				data: resp,
-			  },
+				success: true,
+				msg: "get Login success",
+				data: {
+					page: page,
+					per_page: perPage,
+					total_pages: totalPages,
+					total: total,
+					data: resp,
+				},
 			});
 		} else {
 			res.status(404).json({ success: false, msg: "404 Not Found" })
 		}
+	} catch (err) {
+		console.log({ msg: "on login controller", error: err })
+		res.status(500).json({ success: false, msg: "something went wrong!" })
+	}
+}
+
+exports.updateLogin = async (req, res) => {
+	try {
+		const isAdmin = await Login.findOne({ where: { id: req.user.id, permission: 'admin' } })
+		const { username, password, permission } = req.body;
+
+		if (isAdmin != null) {
+			await Login.update({ username, password, permission }, { where: { id: req.params.id } })
+			res.status(200).json({ success: true, msg: "update login success" })
+		} else {
+			res.status(404).json({ success: false, msg: "404 Not Found" })
+		}
+
+	} catch (err) {
+		console.log({ msg: "on login controller", error: err })
+		res.status(500).json({ success: false, msg: "something went wrong!" })
+	}
+}
+
+exports.deleteLogin = async (req, res) => {
+	try {
+		const isAdmin = await Login.findOne({ where: { id: req.user.id, permission: 'admin' } })
+
+		if (isAdmin != null) {
+			await Login.update({isActive: 0},{where: { id: req.params.id } })
+			res.status(200).json({ success: true, msg: `delete login id: ${req.params.id} successfuly` })
+		} else {
+			res.status(404).json({ success: false, msg: "404 Not Found" })
+		}
+
 	} catch (err) {
 		console.log({ msg: "on login controller", error: err })
 		res.status(500).json({ success: false, msg: "something went wrong!" })
--- api/routes/admin/login.route.js
+++ api/routes/admin/login.route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { validateToken } = require("../../middlewares/auth.middleware");
-const { createLogin, getLogin} = require("../../controllers/admin/login.controller");
+
 // Load Controllers
-router.post("/create", validateToken, createLogin);
+const { createLogin, getLogin, updateLogin, deleteLogin} = require("../../controllers/admin/login.controller");
+
 router.get("/get", validateToken, getLogin);
-
-
+router.post("/create", validateToken, createLogin);
+router.patch("/update/:id", validateToken, updateLogin);
+router.patch("/delete/:id", validateToken, deleteLogin);
 
 module.exports = router;

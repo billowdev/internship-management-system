433aff11eb1f887ec282cf45317f5030852bfcb2
Akkarapon Phikulsri <lacakp.contact@gmail.com>
2022-04-24 16:46:27 +0700

    get student profile

--- api/controllers/students.controller.js
+++ api/controllers/students.controller.js
@@ -1,12 +1,24 @@
-const { Students, Login } = require("../models/internship");
+const { Students, Login, ContactPersons, Educations, PresentAddresses, HometownAddresses, Addresses } = require("../models/internship");
 
 exports.getStudents = async (req, res) => {
 	try {
 		const isStudent = await Login.findOne({ where: { id: req.user.id, roles: 'student' } })
 
 		if (isStudent != null) {
-			const resp = await Students.findOne({ where: { login_id: isStudent.id } })
-			res.status(200).json({ success: true, msg: "get profile success", data:resp })
+			const student = await Students.findOne({ where: { login_id: isStudent.id } })
+			const contactPerson = await ContactPersons.findOne({ where: { student_id: student.id } })
+			const education = await Educations.findOne({ where: { student_id: student.id } });
+			const presentAddr = await PresentAddresses.findOne({ where: { student_id: student.id } })
+			const PersentAddress = await Addresses.findOne({ where: { id: presentAddr.address_id } })
+			const hometownAddr = await HometownAddresses.findOne({ where: { student_id: student.id } })
+			const HometownAddress = await Addresses.findOne({ where: { id: hometownAddr.address_id } })
+
+			if (education != null) {
+				res.status(200).json({ success: true, msg: "get student success", data: { student, contactPerson, education, PersentAddress, HometownAddress } })
+			} else {
+				res.status(200).json({ success: true, msg: "get student success", data: { student, contactPerson, education: {}, PersentAddress, HometownAddress } })
+			}
+
 		} else {
 			res.status(400).json({ success: false, msg: "something wen wrong" })
 		}
